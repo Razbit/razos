63ca8aeb0e9e227dd8574ac9c1e791a39e1cb855
Razbit <razclocker@gmail.com>
2014-11-01 15:23:26 +0200

    Add sprintf() to the kernel lib

---
+++ src/lib/sprintf.c
@@ -0,0 +1,20 @@
+/* This file is a part of the RazOS project
+ *
+ * Razbit 2014 */
+
+#include "stdarg.h"
+#include "string.h"
+#include "vsprintf.h"
+#include "sprintf.h"
+
+int sprintf(char* str, const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    
+    int ret = vsprintf(str, fmt, args);
+
+    va_end(args);
+
+    return ret;
+}
---
+++ src/lib/sprintf.h
@@ -0,0 +1,14 @@
+/* This file is a part of the RazOS project
+ *
+ * Razbit 2014 */
+
+#ifndef SPRINTF_H
+#define SPRINTF_H
+
+#include "stdarg.h"
+#include "string.h"
+#include "vsprintf.h"
+
+int sprintf(char* str, const char* fmt, ...);
+
+#endif /* SPRINTF_H */
