6e0bbaddb7f1297c5262da6ab7ecaa3865724a70
Razbit <razclocker@gmail.com>
2014-12-31 18:55:31 +0200

    oops in sys/types.h

--- src/include/sys/types.h
+++ src/include/sys/types.h
@@ -15,17 +15,17 @@
 
 #ifndef SSIZE_T
 #define SSIZE_T
-typedef long int ssize_t;
+typedef int32_t ssize_t;
 #endif
 
 #ifndef OFF_T
 #define OFF_T
-typedef long int off_t;
+typedef int32_t off_t;
 #endif
 
-typedef long unsigned int gid_t;
-typedef long unsigned int uid_t;
-typedef long unsigned int ino_t;
-typedef long int off_t;
+typedef uint32_t gid_t;
+typedef uint32_t uid_t;
+typedef uint32_t ino_t;
+typedef uint32_t mode_t;
 
 #endif /* TYPES_H */
