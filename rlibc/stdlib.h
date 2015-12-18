3fb74d9f3ab40ccedb153af77492d31f80fde69a
Razbit <razclocker@gmail.com>
2015-12-18 18:16:54 +0200

    exit() in stdlib

---
+++ rlibc/stdlib.h
@@ -0,0 +1,12 @@
+/* This file is a part of the RazOS project
+ *
+ * stdlib.h -- C99-ish stdlib
+ *
+ * Razbit 2015 */
+
+#ifndef STDLIB_H
+#define STDLIB_H
+
+void exit(int status); /* int arch/../crt0.s */
+
+#endif /* STDLIB_H */
